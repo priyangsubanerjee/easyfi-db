1b2040774f03bf1d482edde80e70a91b1e4ff735
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-03-20 02:20:06 +0530

    uploads cleares

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
