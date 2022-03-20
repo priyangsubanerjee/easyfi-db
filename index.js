c688ac8f90432819db805a3e885be87d0b3d99b9
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-03-20 23:12:29 +0530

    front-end modified

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const port = process.env.PORT || 3000;
+const multiparty = require("multiparty");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,6 +20,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
@@ -25,14 +28,30 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/", upload.single("userImage"), (req, res) => {
+app.post("/delete", (req, res) => {
+  const fs = require("fs");
+  const { name } = req.body;
+  var dir = "uploads/" + name;
+
+  if (fs.existsSync(dir)) {
+    fs.rmSync("uploads/" + name, {
+      recursive: true,
+      force: true,
+    });
+  } else {
+    res.send("not found");
+  }
+  res.send("deleted");
+});
+
+app.post("/upload-image", upload.single("file"), (req, res) => {
   console.log(req);
   const url = req.protocol + "://" + req.get("host") + "/";
   res.status(200).json({
-    imageUrl: url + req.file.path,
-    imageName: req.file.originalname,
-    imageType: req.file.mimetype,
-    imageSize: req.file.size / 1000 + "kb",
+    fileUrl: url + req.file.path,
+    fileName: req.file.originalname,
+    fileType: req.file.mimetype,
+    fileSize: req.file.size / 1000 + "kb",
   });
 });
 
---
+++ public/scripts/main.js
@@ -0,0 +1,235 @@
+class File {
+  constructor(name, url, date) {
+    this.name = name;
+    this.url = url;
+    this.date = date;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getUrl() {
+    return this.url;
+  }
+
+  getDate() {
+    return this.date;
+  }
+}
+
+// Variables
+
+const uploadedFilesContainer = document.getElementById("uploaded-files");
+const uploadedFilesContainerEmpty = document.getElementById(
+  "uploaded-files-empty"
+);
+
+const fileUploadInput = document.getElementById("file-upload-input");
+const fileUploadBox = document.getElementById("file-upload-box");
+
+const closeUploadModal = document.getElementById("close-upload-modal");
+const uploadingParentLayer = document.getElementById("uploading-parent-layer");
+const uploadingContainer = document.getElementById("uploading-container");
+const uploadedContainer = document.getElementById("uploaded-container");
+const fileUploadedAnchor = document.getElementById("file-uploaded-anchor");
+
+const askBeforeDeleting = document.getElementById("ask-before-deleting");
+const showQuickMenu = document.getElementById("show-modal-while-uploading");
+
+// Event Listeners
+
+fileUploadBox.addEventListener("click", () => {
+  fileUploadInput.click();
+});
+
+fileUploadInput.addEventListener("change", (e) => {
+  if (e.target.files.length > 0) {
+    if (showQuickMenu.checked) {
+      uploadingParentLayer.style.display = "flex";
+      uploadingContainer.style.display = "block";
+      uploadedContainer.style.display = "none";
+    }
+
+    const file = e.target.files[0];
+    const fileSize = file.size / 1024 / 1024;
+    if (fileSize > 20) {
+      alert("File size is too big. Max file size is 20MB");
+      uploadingParentLayer.style.display = "none";
+      uploadingContainer.style.display = "none";
+      uploadedContainer.style.display = "block";
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", file);
+    axios.post("/upload-image", formData).then(function (response) {
+      if (showQuickMenu.checked) {
+        uploadedContainer.style.display = "block";
+        uploadingContainer.style.display = "none";
+        fileUploadedAnchor.href = response.data.fileUrl;
+        fileUploadedAnchor.innerHTML = response.data.fileName;
+      }
+
+      const file = new File(
+        response.data.fileName,
+        response.data.fileUrl,
+        new Date().toLocaleString()
+      );
+
+      saveFileToLocalStorage(file);
+    });
+  }
+});
+
+closeUploadModal.addEventListener("click", () => {
+  uploadingParentLayer.style.display = "none";
+  uploadingContainer.style.display = "block";
+  uploadedContainer.style.display = "none";
+  document.body.style.overflow = "auto";
+});
+
+uploadedFilesContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    if (askBeforeDeleting.checked) {
+      if (!confirm("Are you sure you want to delete this file?")) {
+        return;
+      }
+    }
+
+    const files = localStorage.getItem("files")
+      ? JSON.parse(localStorage.getItem("files"))
+      : [];
+
+    const fileUrl = e.target.getAttribute("file-url");
+    const index = files.findIndex((file) => file.url === fileUrl);
+    files.splice(index, 1);
+    localStorage.setItem("files", JSON.stringify(files));
+    updateUploads();
+    deleteFile(fileUrl);
+  } else if (e.target.classList.contains("copy")) {
+    const fileUrl = e.target.getAttribute("file-url");
+    copyToClipboard(fileUrl, e.target);
+  }
+});
+
+askBeforeDeleting.addEventListener("click", () => {
+  if (askBeforeDeleting.checked) {
+    localStorage.setItem("askBeforeDeleting", true);
+  } else {
+    localStorage.setItem("askBeforeDeleting", false);
+  }
+});
+
+showQuickMenu.addEventListener("click", () => {
+  if (showQuickMenu.checked) {
+    localStorage.setItem("showQuickMenu", true);
+  } else {
+    localStorage.setItem("showQuickMenu", false);
+  }
+});
+
+// Arrow functions
+
+const getTemplate = (file) => {
+  const template = `
+    <div class="flex items-center border-b px-6 py-3">
+        <div class="items-center whitespace-nowrap">
+            <p class="font-medium">${
+              file.getName().length > 20
+                ? file.getName().substring(0, 17) + "..."
+                : file.getName()
+            }</p>
+            <p class="text-gray-500 text-xs mt-1">(${file.getDate()})</p>
+        </div>
+        <div class="w-full px-6 text-sm text-gray-400 text-center">
+            <a target="_blank" href="${file.getUrl()}">${
+    file.getUrl().substring(0, 50) + "..."
+  }</a>
+        </div>
+        <div class="whitespace-nowrap flex items-center cursor-pointer">
+            <img src="./icons/copy.png" file-url="${file.getUrl()}" class="w-5 h-5 mr-4 copy" alt="">
+            <img src="./icons/delete.png" file-url="${file.getUrl()}" class="w-5 h-5 mr-3 delete" alt="">
+        </div>
+    </div>`;
+
+  return template;
+};
+
+const saveFileToLocalStorage = (file) => {
+  const files = localStorage.getItem("files")
+    ? JSON.parse(localStorage.getItem("files"))
+    : [];
+
+  files.push(file);
+  localStorage.setItem("files", JSON.stringify(files));
+  updateUploads();
+};
+
+const updateUploads = () => {
+  const files = localStorage.getItem("files")
+    ? JSON.parse(localStorage.getItem("files"))
+    : [];
+
+  if (files.length > 0) {
+    uploadedFilesContainer.innerHTML = "";
+    uploadedFilesContainerEmpty.style.display = "none";
+    files.forEach((file) => {
+      uploadedFilesContainer.innerHTML += getTemplate(
+        new File(file.name, file.url, file.date)
+      );
+    });
+  } else {
+    uploadedFilesContainer.innerHTML = "";
+    uploadedFilesContainerEmpty.style.display = "flex";
+  }
+};
+
+const deleteFile = (fileUrl) => {
+  const fileName = fileUrl.split("/")[fileUrl.split("/").length - 1];
+  axios.post("/delete", JSON.stringify({ name: fileName }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const copyToClipboard = (fileUrl, img) => {
+  navigator.clipboard.writeText(fileUrl).then(function () {
+    img.src = "./icons/copied.png";
+    setTimeout(() => {
+      img.src = "./icons/copy.png";
+    }, 3000);
+  });
+};
+
+// Window onload
+
+window.onload = () => {
+  const files = localStorage.getItem("files")
+    ? JSON.parse(localStorage.getItem("files"))
+    : [];
+
+  if (files.length > 0) {
+    uploadedFilesContainer.innerHTML = "";
+    uploadedFilesContainerEmpty.style.display = "none";
+    files.forEach((file) => {
+      uploadedFilesContainer.innerHTML += getTemplate(
+        new File(file.name, file.url, file.date)
+      );
+    });
+  } else {
+    uploadedFilesContainer.innerHTML = "";
+    uploadedFilesContainerEmpty.style.display = "flex";
+  }
+
+  askBeforeDeleting.checked = localStorage.getItem("askBeforeDeleting")
+    ? localStorage.getItem("askBeforeDeleting") == "true"
+      ? true
+      : false
+    : true;
+  showQuickMenu.checked = localStorage.getItem("showQuickMenu")
+    ? localStorage.getItem("showQuickMenu") == "true"
+      ? true
+      : false
+    : true;
+};
